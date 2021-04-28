b0c0c58136efe977d9db2142ca523523d8624e7f
Christian Guzman <christian.guzman1@upr.edu>
2021-04-28 17:42:58 -0400

    Added a command line interface.

--- main.py
+++ main.py
@@ -19,14 +19,53 @@ def run_benchmarks(n):
 
 
 if __name__ == '__main__':
-    # for _ in range(50):
-    #     agent = HamCycleWithShortcutsSnakeAgent()
-    #     environment = SnakeEnvironment(agent, benchmark=True)
-    #     environment.run()
-    result = run_benchmarks(10)
-    print(result)
-    sys.stdout = open('output.txt', 'wt')
-    print(result)
+    print('Welcome to the Snake AI game.')
+    while True:
+        option = int(input('''
+What would you like to do?
+    1. Run and display a single game.
+    2. Run a benchmark.
+    3. Exit program.
+:'''))
+        if option == 1:
+            while True:
+                option2 = int(input('''
+Which strategy would you like your agent to follow?
+    1. Choose the coordinate with the minimum distance to the apple (Use a greedy algorithm which uses
+       the manhattan distance as a heuristic).
+    2. Always follow a predetermined hamiltonian cycle.
+    3. Follow a hamiltonian cycle with the possibility of taking shortcuts.
+:'''))
+                if option2 == 1:
+                    agent = ShortestPathSnakeAgent()
+                elif option2 == 2:
+                    agent = HamCycleSnakeAgent()
+                elif option2 == 3:
+                    agent = HamCycleWithShortcutsSnakeAgent()
+                else:
+                    print('Please enter a number between 1 and 3\n')
+                    continue
 
+                SnakeEnvironment(agent).run()
+                print('Successfully completed game?', 'Yes' if agent.completed_game else 'No')
+                print('Its performance measure for this game was:', agent.performance)
+                print('The total steps the snake took were:', agent.total_steps)
+                print('The average steps taken to reach the apple was:', agent.total_steps//agent.apples_eaten)
+                print('\n')
+                break
 
-
+        elif option == 2:
+            n = int(input('Enter the amount of simulations you want to run per agent (Try to keep the number small): '))
+            print('Please wait while the process completes...')
+            result = run_benchmarks(n)
+            print(result)
+            original = sys.stdout
+            sys.stdout = open('output.txt', 'wt')
+            print(result)
+            sys.stdout = original
+            print('The results have been stored in the output.txt file')
+        elif option == 3:
+            exit()
+        else:
+            print('Please enter a number between 1 and 3\n')
+            continue
